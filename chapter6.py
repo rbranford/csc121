347d57f809d5e83170b603506ee84587e330ee2b
Roderic <rodericbranford@students.belhaven.edu>
2020-02-12 12:02:59 -0600

    first commit for chapter 6

--- chapter6.py
+++ chapter6.py
@@ -5,12 +5,10 @@ SCREEN_HEIGHT = 600
   
   
 def draw_grass():
-    """ Draw the ground """
     arcade.draw_lrtb_rectangle_filled(0, SCREEN_WIDTH, SCREEN_HEIGHT / 3, 0, arcade.color.AIR_SUPERIORITY_BLUE)
  
  
 def draw_snow_person(x, y):
-    """ Draw a snow person """
  
     # Draw a point at x, y for reference
     arcade.draw_point(x, y, arcade.color.RED, 5)
@@ -23,18 +21,24 @@ def draw_snow_person(x, y):
     # Eyes
     arcade.draw_circle_filled(x - 15, 210 + y, 5, arcade.color.BLACK)
     arcade.draw_circle_filled(x + 15, 210 + y, 5, arcade.color.BLACK)
- 
+
+def on_draw(delta_time):
+    arcade.start_render()
+
+    draw_grass()
+    draw_snow_person(on_draw.snow_person1_x, 140)
+    draw_snow_person(450, 180)
+
+    
+
+    on_draw.snow_person1_x += 1
+on_draw.snow_person1_x = 150 
  
 def main():
     arcade.open_window(SCREEN_WIDTH, SCREEN_HEIGHT, "Drawing with Functions")
     arcade.set_background_color(arcade.color.DARK_BLUE)
-    arcade.start_render()
- 
-    draw_grass()
-    draw_snow_person(150, 140)
-    draw_snow_person(450, 180)
- 
-    arcade.finish_render()
+    
+    arcade.schedule(on_draw, 1/60)
     arcade.run()
  
  
